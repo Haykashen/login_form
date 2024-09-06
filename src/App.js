9f826c517384f50c118f7cacd49cc1655dbf31ae
Haykashen <Haykashen@yandex.ru>
2024-09-06 21:42:51 +0300

    11

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import LoginForm from './Components/LoginForm/LoginForm';
 
 function App() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [remenber, setRemenber] = React.useState(false);
   return (
-      <LoginForm />
+      <LoginForm 
+        email={email} 
+        setEmail={setEmail}
+        password={password} 
+        setPassword={setPassword}    
+        remenber={remenber}  
+        setRemenber={setRemenber} 
+      />
   );
 }
 
--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -3,21 +3,33 @@ import './LoginForm.css';
 import { FaLock  } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
-const LoginForm = () =>{
+const LoginForm = ({ email, setEmail, password, setPassword,remenber,setRemenber}) =>{
+    function changeEmail(event) {
+        setEmail(event.target.value);
+    }
+
+    function changePassword(event) {
+        setPassword(event.target.value);
+    }
+
+    function changeRemenber(event) {
+        setRemenber(event.target.value);
+    } 
+
     return(
         <div className="wrapper">
             <form className="form-box">
                 <h1>Login</h1>
                 <div className="input-box">
                     <MdEmail className="icon"/>
-                    <input type="email" placeholder="Email" required/>                   
+                    <input type="email" placeholder="Email" value={email} onChange={changeEmail} required/>                   
                 </div>
                 <div className="input-box">
                     <FaLock className="icon"/>
-                    <input type="password" placeholder="Password" required/> 
+                    <input type="password" placeholder="Password" value={password} onChange={changePassword} required/> 
                 </div> 
                 <div className="remember">                   
-                    <label><input type="checkbox"/>Remember me</label>
+                    <label><input type="checkbox" checked={remenber}  onChange={changeRemenber}/>Remember me</label>
                     <a href="#">Forgot password?</a>
                 </div>  
                 <button type="submit" className="button-login">Login</button>  
